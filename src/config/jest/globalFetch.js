28eaef6bbeeb41ff1f8b49739e6685517170071a
Preecha Sattabut <preecha@dotography.com>
2017-06-02 08:04:46 +0000

    add mock response header from api

--- src/config/jest/globalFetch.js
+++ src/config/jest/globalFetch.js
@@ -1,12 +1,17 @@
 global.Headers = () => ({
-    append: () => {}
+    append: () => ({})
 })
 
 global.fetch = jest.fn();
 
 // Helper to mock a success response (only once)
 fetch.mockResponseSuccess = (body) => {
-  fetch.mockImplementationOnce (
-    () => Promise.resolve({json: () => Promise.resolve(JSON.parse(body))})
-  );
-};
+  fetch.mockImplementationOnce(
+    () => Promise.resolve({
+      headers: {
+        get: () => (['application/json'])
+      },
+      json: () => Promise.resolve(JSON.parse(body))
+    })
+  )
+}
