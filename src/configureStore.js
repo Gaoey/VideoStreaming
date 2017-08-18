14aa50656e3250d882dea3c99dff10e11776f008
Supasan Suklim <supasan.game@gmail.com>
2017-08-18 11:58:51 +0700

    Add thunk withExtraArgument

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import axios from 'axios';
 import Sherlockholmes from 'sherlockholmes'
+import { project } from './config'
 import reducers from './reducers'
 import callAPIMiddleware from './middlewares/callAPIMiddleware'
 
 const { inspector } = new Sherlockholmes()
 
 export default (initialState) => {
-  const middlewares = [thunk, callAPIMiddleware, inspector]
+  const middlewares = [
+    thunk.withExtraArgument(axios),
+    callAPIMiddleware
+  ]
+
+  if (project.ENVIRONMENT === 'dev') {
+    middlewares.push(inspector)
+  }
 
   const enhancer = compose(
     applyMiddleware(...middlewares),
