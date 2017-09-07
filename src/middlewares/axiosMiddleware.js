d2e3f63e391ae949751c6d226aff55318bb87964
Supasan Suklim <supasan.game@gmail.com>
2017-09-07 15:31:51 +0700

    Fix axiosMiddleware

--- src/middlewares/axiosMiddleware.js
+++ src/middlewares/axiosMiddleware.js
@@ -4,7 +4,7 @@ import { configToken } from '../config'
 const axiosMiddleWare = store => next => (action) => {
   axios.defaults.responseType = 'json'
   axios.defaults.headers.common['Content-Type'] = 'application/json'
-  axios.defaults.headers.common.Authorization = configToken.getToken(store)
+  axios.defaults.headers.common.Authorization = configToken.getToken(store.getState())
 
   return next(action)
 }
